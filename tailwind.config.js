dfa8de2b2c118d357ef7ec7672e1edd83a88320d
Saquib Anjum <saquibanjum8969@gmail.com>
2024-12-10 03:28:55 +0530

    ____

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}" // Example of adding another folder
   ],
+  
   theme: {
     extend: {},
   },
